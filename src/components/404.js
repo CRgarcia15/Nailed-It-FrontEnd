010f0090219e84379bbfd4ff9bee1ab10daf99df
CRgarcia15 <cg58708@gmial.com>
2022-09-07 18:38:43 -0400

    update to 404, not done

--- src/components/404.js
+++ src/components/404.js
@@ -3,8 +3,11 @@ import React from "react";
 function Err () {
     return   (
         <div className="bg-zinc-300 w-screen h-screen">
-            <h1><strong>Here, have a hammer and start over. A good project is rarely succesful on the first try. </strong></h1>
-            <p>We are sorry, but we couldn't find what you were looking for. We encourage you to keep looking for inovative and exciting projects.</p>
+            <h1><strong>Oops!!</strong></h1>
+            <img src="../public/Tim-Allen-hammer.png" alt=" "/>
+            <h1><strong>Here, have a hammer and start over! A good project is rarely succesful on the first try. </strong></h1>
+            <p>Is ok, you we can help you find your mojo back.</p>
+            <a href="explore">Explore More</a>
         </div>
     )
 }
