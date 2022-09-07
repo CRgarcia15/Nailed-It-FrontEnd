470d87871bbbf0772029ae955ceced5e4d8c08d5
CRgarcia15 <cg58708@gmial.com>
2022-09-07 14:53:24 -0400

    first commit and 404 draft

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Nav from "./components/navBar"
-import Title from "./components/titleCard"
+import Err from './components/404';
 
 function App() {
   return (
     <div id="content" className="bg-zinc-300 w-screen h-screen">
       <Nav />
       <main className="h-full w-4/5 px-20 container mx-auto">
-        <Title />
+       <Err/>
       </main>
     </div>
   )
---
+++ src/components/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Err () {
+    return   (
+        <div className="bg-zinc-300 w-screen h-screen">
+            <h1><strong>Here, have a hammer and start over. A good project is rarely succesful on the first try. </strong></h1>
+            <p>We are sorry, but we couldn't find what you were looking for. We encourage you to keep looking for inovative and exciting projects.</p>
+        </div>
+    )
+}
+
+export default Err
