ec846784689d20aece33f76dfc1e447b0c9ad594
CRgarcia15 <cg58708@gmial.com>
2022-09-08 02:44:23 -0400

    update 404

--- src/components/404.js
+++ src/components/404.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+//img doesn't want to load properly, look for another one.
 function Err () {
     return   (
         <div className="bg-zinc-300 w-screen h-screen">
             <h1><strong>Oops!!</strong></h1>
-            <img src="../public/Tim-Allen-hammer.png" alt=" "/>
+            <img src="./Tim-Allen-wrench.webp" alt=" "/> 
             <h1><strong>Here, have a hammer and start over! A good project is rarely succesful on the first try. </strong></h1>
             <p>Is ok, you we can help you find your mojo back.</p>
             <a href="explore">Explore More</a>
@@ -12,4 +12,4 @@ function Err () {
     )
 }
 
-export default Err
+export default Err;
