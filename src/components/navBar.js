3dacc4e13127e9de5fe22548195fc10f099ce38a
CRgarcia15 <cg58708@gmial.com>
2022-09-10 17:18:36 -0400

    updated navBar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,23 +1,24 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 function nav_Bar() {
   return (
     <header className="font-roboto w-screen h-28 bg-zinc-300 px-10">
       <nav className="w-screen h-1/8 grid grid-cols-12 text-lg text-center pt-10">
         <div className="flex-none w-full h-full text-zinc-600 text-center font-bold">
-          <a className='hover:text-amber-700' href='#YourProjects'>
+          <Link to="/Project/id" className='hover:text-amber-700'>
             <button className="text-amber-700">01.</button> Your Projects
-          </a>
+          </Link>
         </div>
         <div className="flex-none w-full h-full text-zinc-600 text-center font-bold">
-          <a className='hover:text-amber-700' href='#Create'>
+          <Link to="/create" className='hover:text-amber-700'>
             <button className="text-amber-700">02.</button> Create
-          </a>
+          </Link>
         </div>
         <div className="flex-none w-full Sh-full text-zinc-600 text-center font-bold">
-          <a className='hover:text-amber-700' href='#Explore'>
+          <Link to="" className='hover:text-amber-700'>
             <button className="text-amber-700">03.</button> Explore
-          </a>
+          </Link>
         </div>
       </nav>
     </header>
