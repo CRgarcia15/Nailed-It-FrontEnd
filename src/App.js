16078b2f8567c5117468cdc6308ea0c6f64370e0
CRgarcia15 <cg58708@gmial.com>
2022-09-11 11:22:50 -0400

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Nav from "./components/navBar"
 import Title from "./components/titleCard"
 import Form from "./components/createProject"
-import Project from "./components/singleProject"
+import GridView from './components/GridView'
 import { useState, useEffect } from "react"
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Title />} />
             <Route path="/create" element={<Form />} />
-            <Route path="/Project/id" element={<Project />} />
+            <Route path="/Project/id" element={<GridView />} />
           </Routes>
           
         </main>
