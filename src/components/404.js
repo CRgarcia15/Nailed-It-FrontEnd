c5432a12d1604cbb4ef34604e104690d3c2f1da9
CRgarcia15 <cg58708@gmial.com>
2022-09-14 13:36:35 -0400

    updated 404 route to explore

--- src/components/404.js
+++ src/components/404.js
@@ -14,7 +14,7 @@ function Err () {
             <div className="flex flex-col w-4/5 h-1/3 mx-auto mt-40 mr-40 mt-10">
                 <h1 className='font-roboto text-zinc-500 text-2xl flex shrink mt-5 font-bold'>It'll buff out! A good project is rarely succesful on the first try.</h1>
                 <p className='font-roboto text-zinc-500 text-2xl flex shrink mt-5 '>Is ok, you we can help you find your mojo back.</p>
-                <a href="/" className=" w-1/5 text-amber-700 text-center mt-5 bg-zinc-300 hover:bg-amber-700 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Explore More</a>
+                <a href="/explore" className=" w-1/5 text-amber-700 text-center mt-5 bg-zinc-300 hover:bg-amber-700 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Explore More</a>
            </div>
         </div>
     )
