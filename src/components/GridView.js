d2c1c42e3d32c2350e6155125c27319b74cc271a
CRgarcia15 <cg58708@gmial.com>
2022-09-13 22:01:57 -0400

    got to map, need to display individual project data

--- src/components/GridView.js
+++ src/components/GridView.js
@@ -2,21 +2,30 @@ import React from "react";
 import {useState, useEffect} from 'react';
 
 function GridView () {
-    const [ project, setProject ] = useState({});
+    const [ project, setProject ] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:8080/projects/1")
+        fetch("http://localhost:8080/projects")
             .then((res) => res.json())
             .then((project) => setProject(project))
     }, [])
+    console.log(project)
 
-    return(
-        <div>
+    const display = project.map(() => {
+        return (
+            <div>
             <img src="http://placekitten.com/200/300" alt=""/>
             <h1><strong>Name:</strong> {project.name}</h1>
             <h3><strong>Category:</strong> {project.category}</h3>
             <h3><strong>Time:</strong> {project.time}</h3>
             <h3><strong>Cost:</strong> {project.cost}</h3>
+         </div>
+        )
+    })
+
+    return(
+        <div>
+            {display}
          </div>
     )
 }
