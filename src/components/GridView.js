9167b358d41e7b11bbd58365e95599f471abf377
CRgarcia15 <cg58708@gmial.com>
2022-09-15 19:59:48 -0400

    complete grid view styling

--- src/components/GridView.js
+++ src/components/GridView.js
@@ -16,13 +16,13 @@ function GridView () {
         <div>
             <div className="w-full h-full grid grid-cols-3 space-x-10 mt-20 ">
                 {projects.map((project, index) => {
-                    return <div className="w-50 h-50 hover:shadow-xl hover:bg-zinc-400 hover:transition mx-1.5 bg-zinc-300 rounded-md px-5 py-2" key={index}>
+                    return <div className="w-50 h-50 hover:shadow-xl hover:bg-zinc-400 hover:transition mx-1.5 bg-zinc-300 rounded-md px-5 py-2 text-zinc-500 text-center text-xl" key={index}>
                         <Link to={`/project/${project._id}`} >
                         <ul>
-                            <li className="text-4xl font-semibold">{project.name}</li>
-                            <li>Category: {project.category}</li>
-                            <li>cost: ${project.cost}</li>
-                            <li>Time: {project.time} hour(s)</li>
+                            <li className="text-6xl font-semibold text-zinc-700">{project.name}</li>
+                            <li><strong>Category:</strong> {project.category}</li>
+                            <li><strong>Cost:</strong> ${project.cost}</li>
+                            <li><strong>Time:</strong> {project.time} hour(s)</li>
                         </ul>
                         </Link>
                     </div>
