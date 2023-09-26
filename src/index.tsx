3a5c21d4026cc6b349d722ceb167498f96f6cd03
CRgarcia15 <cg58708@gmial.com>
2023-09-26 19:58:33 -0400

    begin migration

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root: HTMLElement | null = document.getElementById('root');
+const rootContainer = ReactDOM.createRoot(root!)
+rootContainer.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
@@ -14,4 +15,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
